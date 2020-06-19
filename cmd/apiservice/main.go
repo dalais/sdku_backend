727e1bc06379e59fccf29b5f612d35566aca5367
Dalay Salchak <okeanol777@gmail.com>
2020-06-19 13:48:42 +0700

    first commit

---
+++ cmd/apiservice/main.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+)
+
+// Product ... We will first create a new type called Product
+// This type will contain information about VR experiences
+type Product struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Slug        string `json:"slug"`
+	Description string `json:"description"`
+}
+
+// TokenObj ...
+type TokenObj struct {
+	Token string `json:"token"`
+}
+
+// CustJwtMiddleware ...
+type CustJwtMiddleware struct {
+	*jwtmiddleware.JWTMiddleware
+}
+
+// mySigningKey ... Глобальный секретный ключ
+var mySigningKey = []byte("secret123")
+
+/* We will create our catalog of VR experiences and store them in a slice. */
+var products = []Product{
+	{ID: 1, Name: "World of Authcraft", Slug: "world-of-authcraft", Description: "Battle bugs and protect yourself from invaders while you explore a scary world with no security"},
+	{ID: 2, Name: "Ocean Explorer", Slug: "ocean-explorer", Description: "Explore the depths of the sea in this one of a kind underwater experience"},
+	{ID: 3, Name: "Dinosaur Park", Slug: "dinosaur-park", Description: "Go back 65 million years in the past and ride a T-Rex"},
+	{ID: 4, Name: "Cars VR", Slug: "cars-vr", Description: "Get behind the wheel of the fastest cars in the world."},
+	{ID: 5, Name: "Robin Hood", Slug: "robin-hood", Description: "Pick up the bow and arrow and master the art of archery"},
+	{ID: 6, Name: "Real World VR", Slug: "real-world-vr", Description: "Explore the seven wonders of the world in VR"}}
+
+func main() {
+	// Инициализируем gorilla/mux роутер
+	r := mux.NewRouter()
+
+	// Страница по умолчанию для нашего сайта это простой html.
+	r.Handle("/", http.FileServer(http.Dir("./views/")))
+	// Наше API состоит из трех роутов
+	// /status - нужен для проверки работоспособности нашего API
+	// /products - возвращаем набор продуктов,
+	// по которым мы можем оставить отзыв
+	// /products/{slug}/feedback - отображает фидбек пользователя по продукту
+	r.Handle("/auth/jwt", GetTokenHandler).Methods("GET")
+	sr := r.PathPrefix("/api/").Subrouter()
+	sr.Handle("/status", StatusHandler).Methods("GET")
+	sr.Handle("/products", jwtMiddleware.Handler(ProductsHandler)).Methods("GET")
+	sr.Handle("/products/{slug}/feedback", NotImplemented).Methods("POST")
+
+	// Статику (картинки, скрипти, стили) будем раздавать
+	// по определенному роуту /static/{file}
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/",
+		http.FileServer(http.Dir("./static/"))))
+	// Наше приложение запускается на 8000 порту.
+	// Для запуска мы указываем порт и наш роутер
+	http.ListenAndServe(":8000", r)
+}
+
+// NotImplemented ...
+var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Not Implemented"))
+})
+
+// StatusHandler ... The status handler will be invoked when the user calls the /status route
+//  It will simply return a string with the message "API is up and running"
+var StatusHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("API is up and running"))
+})
+
+// ProductsHandler ... The products handler will be called when the user makes a GET request to the /products endpoint.
+// This handler will return a list of products available for users to review
+var ProductsHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// Here we are converting the slice of products to JSON
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+})
+
+// AddFeedbackHandler ... The feedback handler will add either positive or negative feedback to the product
+// We would normally save this data to the database - but for this demo, we'll fake it
+// so that as long as the request is successful and we can match a product to our catalog of products we'll return an OK status.
+var AddFeedbackHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	var product Product
+	vars := mux.Vars(r)
+	slug := vars["slug"]
+
+	for _, p := range products {
+		if p.Slug == slug {
+			product = p
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if product.Slug != "" {
+		payload, _ := json.Marshal(product)
+		w.Write([]byte(payload))
+	} else {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	}
+})
+
+// GetTokenHandler ...
+var GetTokenHandler = http.HandlerFunc(func(w http.ResponseWriter,
+	r *http.Request) {
+	// Создаем новый токен
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	// Устанавливаем набор параметров для токена
+	claims["admin"] = true
+	claims["name"] = "Adminushka"
+	claims["exp"] = time.Now().Add(time.Hour * 24 * 30).Unix()
+
+	// Подписываем токен нашим секретным ключем
+	tokenString, _ := token.SignedString(mySigningKey)
+
+	tokenObj := TokenObj{
+		Token: tokenString,
+	}
+	// Отдаем токен клиенту
+	addCookie(w, "access_token", tokenString, 30*24*time.Hour)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tokenObj)
+})
+
+var custJwtMiddle CustJwtMiddleware
+
+// jwtMiddleware ...
+var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+	Extractor: custJwtMiddle.FromCookie,
+	ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+		return mySigningKey, nil
+	},
+	SigningMethod: jwt.SigningMethodHS256,
+})
+
+// FromCookie ...
+func (jwtmiddleware CustJwtMiddleware) FromCookie(r *http.Request) (string, error) {
+	authHeader := r.Header.Get("Cookie")
+	if authHeader == "" {
+		return "", nil
+	}
+
+	cookie, _ := r.Cookie("access_token")
+	if cookie == nil {
+		return "", nil
+	}
+
+	return cookie.Value, nil
+}
+
+// addCookie will apply a new cookie to the response of a http request
+// with the key/value specified.
+func addCookie(w http.ResponseWriter, name, value string, ttl time.Duration) {
+	expire := time.Now().Add(ttl)
+	cookie := http.Cookie{
+		Name:     name,
+		Value:    value,
+		Expires:  expire,
+		Path:     "/",
+		HttpOnly: true,
+	}
+	http.SetCookie(w, &cookie)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/dalais/sdku_backend
+
+go 1.14
+
+require (
+	github.com/auth0/go-jwt-middleware v0.0.0-20200507191422-d30d7b9ece63
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/mux v1.7.4
+)
